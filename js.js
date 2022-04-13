bc4cc1ab4a9b409193ed58a17f85537e83cb5c27
shervin nj <nj_shervin@yahoo.com>
2022-04-13 17:48:44 -0500

    array-object

---
+++ js.js
@@ -0,0 +1,17 @@
+// let number=[5,9,1,"opencode","true"];
+// number[0]=15;
+
+// console.log(number);
+let person={
+name:"shervin",
+age:20,
+gender:"male",
+address:{
+    country:"iran",
+    city:"tehran"
+},
+hobbies:["watching","movies","codding"]
+
+};
+person.email="nj_shervin@yahoo.com";
+console.log(person);
