30beff0ebdfd0d8e6f29489d7b2bc05f67d305de
shervin nj <nj_shervin@yahoo.com>
2022-04-14 14:41:59 -0500

    array object

--- js.js
+++ js.js
@@ -2,16 +2,63 @@
 // number[0]=15;
 
 // console.log(number);
-let person={
-name:"shervin",
-age:20,
-gender:"male",
-address:{
-    country:"iran",
-    city:"tehran"
-},
-hobbies:["watching","movies","codding"]
+// let person={
+// name:"shervin",
+// age:20,
+// gender:"male",
+// address:{
+//     country:"iran",
+//     city:"tehran"
+// },
+// hobbies:["watching","movies","codding"]
 
-};
-person.email="nj_shervin@yahoo.com";
-console.log(person);
+// };
+// person.email="nj_shervin@yahoo.com";
+// console.log(person);
+
+
+
+// function sayHello(name,age){
+//     console.log("hello my name is "+name+age);
+// }
+// sayHello("shervin",26);
+// sayHello("sherv",26);
+// sayHello("shevin",26);
+
+// function square(number){
+// return number*number
+// }
+// console.log(square(2));
+
+
+
+
+// function sayHello(name,age){
+
+//     return name*age
+// }
+// document.write(sayHello(2,5)); 
+// function sayHello(name,age){
+//     return name*age
+
+
+// }
+// console.log(sayHello(2,5))
+
+// let family={name:"shervin",last:"nj"}
+// let as=family;
+
+
+//     console.log(family);
+//     console.log(as);
+//______________________________________
+// let array=[1,2,3,4];
+// let oarray=array;
+// oarray=Object.assign([],array);
+// oarray[1]=5;
+
+// console.log(array);
+// console.log(oarray);
+//alan ye log begiri 2 tash tagir karde ma fagat 
+//mikhayim yekishoon tagir koneh
+//____________________________________________________
